c39eb0e10c2f97f2abd87b71af4bd49bd86e1465
Ricardo Antunes_Vieira <ricardo_av18@hotmail.com>
2021-02-24 10:04:57 -0300

    Portfolio In Construction

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,14 @@
-import { useState } from 'react';
 function Home() {
-    return (
-        <div>
-            <h1> Home 3</h1>
-            <Contador />
-        </div>
-    )
+  return <div>
+
+    <h1>
+      Portfolio @RVDev em contrução !!!
+    </h1>
+    <h3 className="link">
+      Contact <a href="https://www.linkedin.com/in/ricardoav/">Linkedin</a>
+    </h3>
+  </div>
 }
 
-function Contador() {
-    const [contador, setContdor] = useState(1);
-
-    function adicionarContador() {
-        setContdor(contador + 1);
-
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
-
-google-site-verification: google8bf91f43717cb7f2.html
