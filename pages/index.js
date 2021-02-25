f6854c3d3869a629e146551698dd5e910b631730
Ricardo Antunes_Vieira <ricardo_av18@hotmail.com>
2021-02-25 08:23:51 -0300

    Portfolio 1.0.1

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return <div>
 
     <h1>
-      Portfolio @RVDev em construção !!!!
+      Portfolio @RVDev em construção !!!
     </h1>
     <h3 className="link">
       Contact <a href="https://www.linkedin.com/in/ricardoav/">Linkedin</a>
