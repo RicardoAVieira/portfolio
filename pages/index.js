ab6ff74615787bad1fee4fb68e56473e60ea2879
Ricardo Antunes_Vieira <ricardo_av18@hotmail.com>
2021-02-25 14:50:34 -0300

    Add Git to repository

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,14 @@ function Home() {
     <h1>
       Portfolio @RVDev em construção !!!
     </h1>
+    <h3 class name="link">
+      Sources <a href="https://github.com/RicardoAVieira">GitHub</a>
+    </h3>
+
     <h3 className="link">
       Contact <a href="https://www.linkedin.com/in/ricardoav/">Linkedin</a>
     </h3>
+
   </div>
 }
 
