cdff4e39f08fa9399ca2b4b0610eac2c9f2af184
Ricardo Antunes_Vieira <ricardo_av18@hotmail.com>
2021-03-03 17:49:31 -0300

    1.0.1 - Photo scroll adjustment

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
@@ -10,20 +10,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+
       <main className={styles.main}>
-
-
-        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div class="md:flex">
+        <div class="max-w-xl mx-auto bg-white rounded-x1 shadow-md overflow-hidden md:max-w-2xl">
+          <div class="md:flex w-auto h-auto ">
             <div class="md:flex-shrink-0">
-              <img class="h-48 w-full object-cover md:w-48" src="/20210117_1510182.jpg" alt="Author Image" />
+              <img class="h-auto w-auto object-cover lg:w-48" src="/20210117_1510182.jpg" alt="Author Image" />
             </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Ricardo Antunes Vieira</div>
-              <a href="mailto:ricardo.antunes.vieira@gmail.com" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding new jobs</a>
-              <p class="mt-2 text-gray-500">
+            <div class="p-5 ">
+              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold ">Ricardo Antunes Vieira</div>
+              <a href="mailto:ricardo.antunes.vieira@gmail.com"
+                class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Contact for new jobs</a>
+              <p class="mt-2 text-gray-600 ">
                 Professional in the Information Technology area, focused on mobile and web development.
               </p>
+
             </div>
           </div>
         </div>
